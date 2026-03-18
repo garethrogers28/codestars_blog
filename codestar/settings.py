661c602621261b0d294f1b4b470cc17b656964f8
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-03-18 13:21:13 +0000

    Update allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)1(@6)ta4riwcxl-)ba#l207(wgkaez$ks26g#p3nb^+4yz)37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','.herokuapp.com',]
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com',]
 
 
 # Application definition
