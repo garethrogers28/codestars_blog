66e5c177cfd1e1eea7d43aca49b389a67fbce859
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-03-22 15:07:11 +0000

    Connected to database and created superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)1(@6)ta4riwcxl-)ba#l207(wgkaez$ks26g#p3nb^+4yz)37'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com',]
 
