1111474282d84f3f385eacd3156dc9b3ab841755
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-03-26 13:31:05 +0000

    Migration Practice

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
     def __str__(self):
@@ -29,6 +30,8 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+
     class Meta:
         ordering = ["created_on"]
     def __str__(self):
