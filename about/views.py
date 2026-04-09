42024d6715c67972fd73511e1ce967097638a03f
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-04-09 12:48:43 +0100

    Wired up post request in about views

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import About
 from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,7 +10,21 @@ def about_me(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+
+    if request.method =="POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+        request, messages.SUCCESS,
+        "Collaboration request received! I endeavour to respond within 2 working days."
+    )
+
+
+
     collaborate_form = CollaborateForm()
+    
+
 
     return render(
         request,
